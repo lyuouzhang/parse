3318fa60c92c7b84494b12d70f47546995c004e9
lyuouzhang <78629577+lyuouzhang@users.noreply.github.com>
2024-09-17 13:08:51 +0800

    Add files via upload

---
+++ R/data_islet.R
@@ -0,0 +1,26 @@
+#' @name islet
+#' @aliases islet
+#'
+#' @title Gene-expression Data for Asthma Disease
+#' @description The data contains gene expression data of 108 objects.
+#' @usage data(islet)
+#' @docType data
+#' @format
+#' \describe{
+#'  \item{Rows}{Objects}
+#'  \item{Columns}{Genes}
+#' }
+#'
+#' @keywords
+#' dataset
+#' @examples
+#' data(islet_covariate1)
+#' data(islet_covariate2)
+#' data(islet_covariate3)
+#' data(islet_covariate4)
+#' data(islet_gene)
+#' data(islet_label)
+#' islet_covariate = cbind(islet_covariate1,islet_covariate2,islet_covariate3,islet_covariate4)
+#' colnames(islet_covariate) = islet_gene
+
+"islet"
